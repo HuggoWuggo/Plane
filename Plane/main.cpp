e7f9b9b1066402ec0bea748c2aa7b79b3e2b7af1
HuggoWuggo <hugo.lewczak@gmail.com>
2024-10-28 07:18:59 +1100

    Code Refactoring

--- Plane/main.cpp
+++ Plane/main.cpp
@@ -3,45 +3,26 @@
 
 #define GRID_SIZE 11
 
-void draw_grid(int gradient, int y_intercept)
-{
-    std::cout << "Y intercept is "<< y_intercept << ". And the Gradient is " << gradient << '\n';
-    for (int y = 0; y < GRID_SIZE; y++)
-    {
-        for (int x = 0; x < GRID_SIZE; x++)
-        {
-            if ((x == std::ceil(GRID_SIZE / 2) + 1) && (y == std::ceil(GRID_SIZE / 2) - y_intercept - gradient))
-            {
-                std::cout << " * ";
-            }
-            else if ((x == std::ceil(GRID_SIZE / 2) - 1) && (y == std::ceil(GRID_SIZE / 2) - y_intercept + gradient))
-            {
-                std::cout << " * ";
-            }
-            else if (x == std::ceil(GRID_SIZE / 2))
-            {
-                if (y != (std::ceil(GRID_SIZE / 2) - y_intercept))
-                {
+void draw_grid(int gradient, int y_intercept) {
+    constexpr int center = GRID_SIZE / 2;
+    std::cout << "Y intercept is " << y_intercept << ". And the Gradient is " << gradient << '\n';
+    
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (x == center) {
+                // Draw vertical axis or plot the y-intercept
+                if (y == center - y_intercept) {
+                    std::cout << " * ";
+                } else {
                     std::cout << " | ";
                 }
-                else if (y_intercept == 0)
-                {
-                    std::cout << " * ";
-                }
-                else if (y_intercept > 0)
-                {
-                    std::cout << " * ";
-                }
-                else if ( y_intercept < 0)
-                {
-                    std::cout << " * ";
-                }
-            } else if (y == std::ceil(GRID_SIZE / 2))
-            {
+            }  else if (y == center - y_intercept - gradient * (x - center)) {
+                // Draw the line based on the equation
+                std::cout << " * ";
+            }else if (y == center) {
+                // Draw horizontal axis
                 std::cout << " - ";
-            }
-            else
-            {
+            } else {
                 std::cout << "   ";
             }
         }
@@ -49,52 +30,29 @@ void draw_grid(int gradient, int y_intercept)
     }
 }
 
-void take_input(int& gradient, int& y_intercept)
-{
-    size_t index = 0;
-    size_t index2 = 0;
-    // y = mx+b
-    std::string inp;
-    std::cout << "Enter equation: ";
-    std::cin >> inp;
+void take_input(int& gradient, int& y_intercept) {
+    std::string equation;
+    std::cout << "Enter equation (format y=mx+b): ";
+    std::cin >> equation;
 
-    if(inp.find('x') == std::string::npos)
-    {
-        std::cout << "There must be an x and y coord\n";
+    if (equation.find("x") == std::string::npos || equation.find('=') == std::string::npos) {
+        std::cerr << "Invalid format. Please enter equation as y=mx+b.\n";
         exit(EXIT_FAILURE);
     }
-    if (inp.find('+') == std::string::npos && inp.find('-') == std::string::npos)
-    {
-        std::cout << "If no y-intercept is given, please add '+0' to the end\n";
-        exit(EXIT_FAILURE);
-    } else
-    {
-            
-        index = inp.find('=');
-        index2 = inp.find('x');
-        std::string x = std::string(&inp[index + 1], &inp[index2+1]);
-    
-        gradient = std::stoi(x);
 
-        if (inp.find('+') != std::string::npos)
-        {
-            index = inp.find('+');
-            y_intercept = inp[index+1] - '0';
-        }
-        else
-        {
-            index = inp.find('-');
-            y_intercept = std::stoi(std::string(&inp[index], &inp[index+2]));
-        }
-        
+    int index_x = equation.find('x');
+    gradient = std::stoi(equation.substr(2, index_x - 2));  // Extracts `m` in y=mx+b
+
+    if (index_x + 1 < equation.size()) {
+        y_intercept = std::stoi(equation.substr(index_x + 1));  // Extracts `b`
+    } else {
+        y_intercept = 0;  // Default to zero if no y-intercept is provided
     }
-
 }
 
-int main(int argc, char* argv[])
-{
-    int gradient = NULL;
-    int y_intercept = NULL;
+int main() {
+    int gradient = 0;
+    int y_intercept = 0;
     take_input(gradient, y_intercept);
     draw_grid(gradient, y_intercept);
     return 0;
