6e1591c5b4b04eb16b40eceaf3334facd1692295
HuggoWuggo <hugo.lewczak@gmail.com>
2024-10-28 16:52:59 +1100

    Add Line Colours for line 2 and fix bug

--- Plane/main.cpp
+++ Plane/main.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
+#include <climits>
 
 #define GRID_SIZE 21
 
-void draw_grid(int gradient, int y_intercept, int gradient_2 = NULL, int y_intercept_2 = NULL) {
+void draw_grid(int gradient, int y_intercept, int gradient_2 = INT_MIN, int y_intercept_2 = INT_MIN) {
     constexpr int center = GRID_SIZE / 2;
     std::cout << "Y intercept is " << y_intercept << ". And the Gradient is " << gradient << '\n';
-    if (gradient_2 != NULL && y_intercept_2 != NULL)
-    {
-        std::cout << "Y intercept is " << y_intercept_2 << ". And the Gradient is " << gradient_2 << '\n';
+    if (gradient_2 != INT_MIN && y_intercept_2 != INT_MIN) {
+        std::cout << "Y intercept of second line is " << y_intercept_2 << ". And the Gradient is " << gradient_2 << '\n';
     }
 
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(consoleHandle, 7);
-    
+
     for (int y = 0; y < GRID_SIZE; y++) {
         for (int x = 0; x < GRID_SIZE; x++) {
-            if (x == center) {
-                // Draw vertical axis or plot the y-intercept
-                if (y == center - y_intercept || (y == center - y_intercept_2 && y_intercept == NULL)) {
-                    if (y_intercept_2 != NULL)
-                    {
-                        SetConsoleTextAttribute(consoleHandle, 9);
-                        std::cout << " * ";
-                        SetConsoleTextAttribute(consoleHandle, 7);
-                    } else
-                    {
-                        SetConsoleTextAttribute(consoleHandle, 7);
-                        std::cout << " * ";
-                    }
+            int y1 = center - y_intercept - gradient * (x - center);     // y-value for first line
+            int y2 = center - y_intercept_2 - gradient_2 * (x - center);  // y-value for second line
+
+            // Check for the intersection point
+            if (gradient_2 != INT_MIN && y_intercept_2 != INT_MIN && y == y1 && y == y2) {
+                SetConsoleTextAttribute(consoleHandle, 12);  // Red color
+                std::cout << " * ";
+                SetConsoleTextAttribute(consoleHandle, 7);    // Reset to default color
+            }
+            else if (x == center) {
+                // Draw vertical axis or plot the y-intercepts
+                if (y == center - y_intercept) {
+                    SetConsoleTextAttribute(consoleHandle, 7);
+                    std::cout << " * ";
+                } else if (y == center - y_intercept_2 && y_intercept_2 != INT_MIN) {
+                    SetConsoleTextAttribute(consoleHandle, 9);  // Different color for second line's y-intercept
+                    std::cout << " * ";
+                    SetConsoleTextAttribute(consoleHandle, 7);
                 } else {
                     std::cout << " | ";
                 }
-            }  else if (y == center - y_intercept - gradient * (x - center)) {
-                // Draw the line based on the equation
+            } else if (y == y1) {
+                // Draw the first line
+                SetConsoleTextAttribute(consoleHandle, 7);
                 std::cout << " * ";
-            } else if (y == center - y_intercept_2 - gradient_2 * (x - center) && gradient_2 != NULL && y_intercept_2 != NULL)
-            {
+            } else if (y == y2 && gradient_2 != INT_MIN) {
+                // Draw the second line
                 SetConsoleTextAttribute(consoleHandle, 9);
                 std::cout << " * ";
                 SetConsoleTextAttribute(consoleHandle, 7);
-            }
-            else if (y == center) {
+            } else if (y == center) {
                 // Draw horizontal axis
                 SetConsoleTextAttribute(consoleHandle, 7);
                 std::cout << " - ";
@@ -54,6 +59,8 @@ void draw_grid(int gradient, int y_intercept, int gradient_2 = NULL, int y_inter
     }
 }
 
+
+
 void take_input(int& gradient, int& y_intercept) {
     std::string equation;
     std::cout << "Enter equation (format y=mx+b): ";
@@ -64,10 +71,10 @@ void take_input(int& gradient, int& y_intercept) {
         exit(EXIT_FAILURE);
     }
 
-    int index_x = equation.find('x');
+    int index_x = static_cast<int>(equation.find('x'));
     gradient = std::stoi(equation.substr(2, index_x - 2));  // Extracts `m` in y=mx+b
 
-    if (index_x + 1 < equation.size()) {
+    if (index_x + 1 < static_cast<int>(equation.size())) {
         y_intercept = std::stoi(equation.substr(index_x + 1));  // Extracts `b`
     } else {
         y_intercept = 0;  // Default to zero if no y-intercept is provided
