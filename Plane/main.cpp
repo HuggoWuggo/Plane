fea522dfa8818f3a41ad5fa1ce58ba412a04cb12
HuggoWuggo <hugo.lewczak@gmail.com>
2024-10-28 11:45:24 +1100

    Add Colours

--- Plane/main.cpp
+++ Plane/main.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
 #include <string>
+#include <Windows.h>
 
 #define GRID_SIZE 21
 
-void draw_grid(int gradient, int y_intercept) {
+void draw_grid(int gradient, int y_intercept, int gradient_2 = NULL, int y_intercept_2 = NULL) {
     constexpr int center = GRID_SIZE / 2;
     std::cout << "Y intercept is " << y_intercept << ". And the Gradient is " << gradient << '\n';
+    if (gradient_2 != NULL && y_intercept_2 != NULL)
+    {
+        std::cout << "Y intercept is " << y_intercept_2 << ". And the Gradient is " << gradient_2 << '\n';
+    }
+
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(consoleHandle, 7);
     
     for (int y = 0; y < GRID_SIZE; y++) {
         for (int x = 0; x < GRID_SIZE; x++) {
             if (x == center) {
                 // Draw vertical axis or plot the y-intercept
-                if (y == center - y_intercept) {
-                    std::cout << " * ";
+                if (y == center - y_intercept || (y == center - y_intercept_2 && y_intercept == NULL)) {
+                    if (y_intercept_2 != NULL)
+                    {
+                        SetConsoleTextAttribute(consoleHandle, 9);
+                        std::cout << " * ";
+                        SetConsoleTextAttribute(consoleHandle, 7);
+                    } else
+                    {
+                        SetConsoleTextAttribute(consoleHandle, 7);
+                        std::cout << " * ";
+                    }
                 } else {
                     std::cout << " | ";
                 }
             }  else if (y == center - y_intercept - gradient * (x - center)) {
                 // Draw the line based on the equation
                 std::cout << " * ";
-            }else if (y == center) {
+            } else if (y == center - y_intercept_2 - gradient_2 * (x - center) && gradient_2 != NULL && y_intercept_2 != NULL)
+            {
+                SetConsoleTextAttribute(consoleHandle, 9);
+                std::cout << " * ";
+                SetConsoleTextAttribute(consoleHandle, 7);
+            }
+            else if (y == center) {
                 // Draw horizontal axis
+                SetConsoleTextAttribute(consoleHandle, 7);
                 std::cout << " - ";
             } else {
                 std::cout << "   ";
@@ -53,7 +77,15 @@ void take_input(int& gradient, int& y_intercept) {
 int main() {
     int gradient = 0;
     int y_intercept = 0;
+    int gradient_2 = 0;
+    int y_intercept_2 = 0;
+    
+    
     take_input(gradient, y_intercept);
     draw_grid(gradient, y_intercept);
+
+    take_input(gradient_2, y_intercept_2);
+    draw_grid(gradient, y_intercept, gradient_2, y_intercept_2);
+    
     return 0;
 }
